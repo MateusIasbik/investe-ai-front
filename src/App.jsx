1fc3333f16b4af9b0e6ec23b030ceba2723dd2e3
Mateus Iasbik <mateusmpi@gmail.com>
2025-04-08 20:28:42 -0300

    fix: token added - test 5

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ export default function App() {
   });
 
   const { token: routeToken } = useParams();
+  console.log('routeToken:', routeToken);
 
   useEffect(() => {
     let storedToken = routeToken;
